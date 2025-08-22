3df1607f2a34c7022197e493c0dd3f72ba165094
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2025-08-21 23:51:55 -0500

    upload improvement

--- front-end/microfrontends/cart_app/src/Cart/Component/listItems.tsx
+++ front-end/microfrontends/cart_app/src/Cart/Component/listItems.tsx
@@ -1,10 +1,24 @@
+import { Product } from "../Model/Product";
 import ItemCard from "./itemCard";
+
+interface ListItemsProps{
+  products:Product[];
+}
 
 interface ListItems {
   shopName: string;
 }
 
-export default function ListItems() {
+export default function ListItems({products}:ListItemsProps) {
+  const grupedByWarehouse = products.reduce<Record<string, Product[]>>(
+    (acc, product)=>{
+      if (!acc[product.warehouse]){
+        acc[product.warehouse] = [];
+      }
+      acc[product.warehouse].push(product);
+      return acc;
+    }, {}
+  );
   return (
     <>
       <div className=" text-3xl">Tu Carrito de Compra</div>
@@ -12,32 +26,23 @@ export default function ListItems() {
       <div className=" text-1x py-3 ">Lugar de compra</div>
 
       <div className="join join-vertical bg-base-100">
-        <div className="collapse collapse-arrow join-item border-base-300 border">
-          <input type="radio" name="my-accordion-4" defaultChecked />
-          <div className="collapse-title font-semibold">Bodega 1</div>
-          <div className="collapse-content text-sm">
-            {" "}
-            <ItemCard
-              productName={"Tiguer special"}
-              productImage={""}
-              productPrice={0}
-              productQuantity={0}
-            ></ItemCard>
+        {Object.entries(grupedByWarehouse).map(([warehouse, warehouseProducts]) => (
+          <div key={warehouse} className="collapse collapse-arrow join-item border-base-300 border">
+            <input type="radio" name="my-accordion-4" />
+            <div className="collapse-title font-semibold">{warehouse}</div>
+            <div className="collapse-content text-sm">
+              {warehouseProducts.map((product) => (
+                <ItemCard
+                  key={product.id}
+                  productName={product.productName}
+                  productImage={product.productImage}
+                  productPrice={product.productPrice}
+                  productQuantity={product.productQuantity}
+                />
+              ))}
+            </div>
           </div>
-        </div>
-        <div className="collapse collapse-arrow join-item border-base-300 border">
-          <input type="radio" name="my-accordion-4" />
-          <div className="collapse-title font-semibold">Bodega 2?</div>
-          <div className="collapse-content text-sm">
-            {" "}
-            <ItemCard
-              productName={"Tiguer special"}
-              productImage={""}
-              productPrice={0}
-              productQuantity={0}
-            ></ItemCard>
-          </div>
-        </div>
+        ))}
 
         <>{/* boton de pagar*/} </>
 
